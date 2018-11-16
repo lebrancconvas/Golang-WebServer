e0a039213b0ae92347dcacf141fa04ed6d6ff9e7
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2018-11-16 17:01:01 +0700

    Add Route form /user

--- main.go
+++ main.go
@@ -2,25 +2,37 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"net/http"
 )
 
 func main() {
+	userDB := map[string]int{
+		"tarwaan":21,
+		"mint":20,
+		"keyboard":17,
+	}
 	http.HandleFunc("/",func(w http.ResponseWriter,r *http.Request) {
 		fmt.Fprintf(w,"Hello Golang Server")
 	})
-	http.HandleFunc("/index",func(w http.ResponseWriter,r *http.Request) {
-		fmt.Fprintf(w,"Index Body")
+
+	// Route
+	http.HandleFunc("/user/",func (w http.ResponseWriter,r *http.Request) {
+		name := r.URL.Path[len("/user/"):]
+		age := userDB[name]
+		fmt.Fprintf(w,"I am %s , My Age is %d",strings.Title(name),age)
 	})
+	
 	http.HandleFunc("/information",func(w http.ResponseWriter,r *http.Request) {
 		fmt.Fprintf(w,"More Information Contact : Scarletto Studio")
 	})
+	
 	http.HandleFunc("/hello",func(w http.ResponseWriter,r *http.Request) {
 		fmt.Fprintf(w,"Hello World")
 	})
+	
 	http.HandleFunc("/wtf",func(w http.ResponseWriter,r *http.Request) {
 		fmt.Fprintf(w,"What The Fuck.")
 	})
 	http.ListenAndServe(":8080",nil)
-	fmt.Println("Go Server Run at :8080")
 }
