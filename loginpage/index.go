9b31daabff9d96c828772fed85d86e9e33b1c7a4
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2018-11-17 22:24:21 +0700

    Login Page.

--- loginpage/index.go
+++ loginpage/index.go
@@ -6,15 +6,34 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/",login)
-	http.HandleFunc("/login",loginpage)
+	http.HandleFunc("/",index)
+	http.HandleFunc("/login",login)
+	http.HandleFunc("/signup",signup)
+	http.HandleFunc("/single",single)
+	http.HandleFunc("/loginpage",loginpage)
 	http.ListenAndServe(":8080",nil)
+}
+
+func index(w http.ResponseWriter,r *http.Request) {
+	http.ServeFile(w,r,"index.html")
 }
 
 func login(w http.ResponseWriter,r *http.Request) {
 	http.ServeFile(w,r,"login.html")
 }
 
+func signup(w http.ResponseWriter,r *http.Request) {
+	http.ServeFile(w,r,"signup.html")
+}
+
+func single(w http.ResponseWriter,r *http.Request) {
+	http.ServeFile(w,r,"single.html")
+}
+
 func loginpage(w http.ResponseWriter,r *http.Request) {
 	fmt.Println("Method : ",r.Method)
+	r.ParseForm()
+	fmt.Println("Username : ",r.Form["username"])
+	fmt.Println("Password : ",r.Form["password"])
+	http.ServeFile(w,r,"loginpage.html")
 } 
